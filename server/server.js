044b8484c6eb9f10d8ed073e8ac552c787379c5a
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2022-12-01 07:54:20 +0530

    route setup with adding and deleting task

--- server/server.js
+++ server/server.js
@@ -5,6 +5,18 @@ const app = express();
 
 app.use(cors());
 
+app.get('/tasks', (req, res)=> {
+    res.send('list all tasks')
+})
+
+app.get('/addTask', (req, res)=> {
+    res.send('You can add task')
+})
+
+app.get('/delete', (req, res)=> {
+    res.send('Deleted task')
+})
+
 app.listen (4000, ()=>
     console.log('running on port 4000')
 
